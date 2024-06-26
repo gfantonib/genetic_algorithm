5227045a0e73e059f2d2ce7c96434f8590434393
gfantoni <guilhermefantoniborges@gmail.com>
2024-06-26 12:28:24 -0300

    Reference column chaged to reference

--- source/_01_oil_database.py
+++ source/_01_oil_database.py
@@ -3,17 +3,17 @@ def oil_database(df):
 	df = df.drop(columns=['Unnamed: 0'])
 
 	# Change column name.
-	df = df.rename(columns={'Unnamed: 19': 'Reference'})
+	df = df.rename(columns={'Unnamed: 19': 'reference'})
 
 	# Create column of reference with A and I.
-	df_ref_column = df['Reference']
+	df_ref_column = df['reference']
 
 	# Store the DataFrame reference of A's and I's in a int.
-	total_A = df["Reference"].value_counts()["A"]
-	total_I = df["Reference"].value_counts()["I"]
+	total_A = df["reference"].value_counts()["A"]
+	total_I = df["reference"].value_counts()["I"]
 
 	# Define operative DataFrame:
-	# with the columns from X11 to X92, without Reference column.
+	# with the columns from X11 to X92, without reference column.
 	df_op = df.loc[:, "X11": "X92"]
 
 	# Count the number of operatives columns (between X11 and X92).
